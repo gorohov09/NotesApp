cb209263aae3c49032cfba797082033ea43018a9
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-23 20:27:58 +0500

    Описание команды и обработчика удаления заметки

---
+++ Notes.Backend/Core/Notes.Application/Notes/Commands/DeleteNote/DeleteNoteCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Commands.DeleteNote
+{
+    public class DeleteNoteCommandHandler : IRequestHandler<DeleteNoteCommand, bool>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public DeleteNoteCommandHandler(INotesDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<bool> Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes.FirstOrDefaultAsync(note => note.Id == request.Id, cancellationToken);
+
+            if (entity is null || entity.UserId != request.UserId)
+                return false;
+
+            _dbContext.Notes.Remove(entity);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
