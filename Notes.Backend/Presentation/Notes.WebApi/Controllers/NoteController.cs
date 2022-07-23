beef5ecb02220c0f8a5b80b9a794cc3849e9ad48
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-23 23:40:30 +0500

    Создание базового контроллера и контроллера заметок

---
+++ Notes.Backend/Presentation/Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Queries.GetNoteList;
+using System.Threading.Tasks;
+
+namespace Notes.WebApi.Controllers
+{
+    [Route("api/note")]
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery
+            {
+                UserId = UserId,
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+    }
+}
