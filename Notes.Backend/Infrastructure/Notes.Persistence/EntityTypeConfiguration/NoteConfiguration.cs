7e777079cdb11e053d42d45000a99734ad876d70
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-23 19:04:54 +0500

    Создание конфигурации для сущности - заметка

---
+++ Notes.Backend/Infrastructure/Notes.Persistence/EntityTypeConfiguration/NoteConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+namespace Notes.Persistence.EntityTypeConfiguration
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id); //Id - первичный ключ
+            builder.HasIndex(note => note.Id).IsUnique(); //Индексирование по Id(уникальный)
+            builder.Property(note => note.Title).HasMaxLength(250); //Огроничение заголовка в 250 символов
+        }
+    }
+}
