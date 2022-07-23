39feb79ee3e94cf9c383455641af954def875887
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-23 19:17:15 +0500

    Класс для создания БД

---
+++ Notes.Backend/Infrastructure/Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated(); //Если БД не создана, она будет создана на основе наше контекста
+        }
+    }
+}
