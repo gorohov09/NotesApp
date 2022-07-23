197442fccc88ad23b8a909198c9a8a83b6c08c4c
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-23 20:01:34 +0500

    Описание обработчика для создания заметки

---
+++ Notes.Backend/Core/Notes.Application/Notes/Commands/CreateNote/CreateNodeCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Commands.CreateNote
+{
+    public class CreateNodeCommandHandler : IRequestHandler<CreateNodeCommand, Guid>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public CreateNodeCommandHandler(INotesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreateNodeCommand request, CancellationToken cancellationToken)
+        {
+            var note = new Note
+            {
+                UserId = request.UserId,
+                Title = request.Title,
+                Details = request.Details,
+                CreationDate = DateTime.Now,
+                EditDate = null
+            };
+
+            await _dbContext.Notes.AddAsync(note, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return note.Id;
+        }
+    }
+}
