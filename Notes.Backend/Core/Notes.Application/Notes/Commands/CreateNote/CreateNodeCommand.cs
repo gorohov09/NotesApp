a051f6559455cb8dd6edc4a7b023474947f55365
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-23 19:54:35 +0500

    Описание команды - создание заметки

---
+++ Notes.Backend/Core/Notes.Application/Notes/Commands/CreateNote/CreateNodeCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+
+namespace Notes.Application.Notes.Commands.CreateNote
+{
+    public class CreateNodeCommand : IRequest<Guid> //Результат выполнение команды - Guid
+    {
+        public Guid UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Details { get; set; }
+    }
+}
